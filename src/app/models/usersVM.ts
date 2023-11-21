f4c8d6ea86e68e12da28b8578b9b62606d14faf2
saimrasool08 <saimrasoolkambo@gmail.com>
2023-11-21 21:11:13 +0500

    send message on Enter Button and new Line with Shift key and Model  structure

--- src/app/models/usersVM.ts
+++ src/app/models/usersVM.ts
@@ -1,7 +1,45 @@
-export class UserVM {
-    userId?: number;  
-    userName?: string;  
-    password?: string;  
-    image?: string;  
+export class UserVM implements IUserVM {
+  userId?: number;
+  userName?: string;
+  password?: string;
+  image?: string;
+  constructor(data?: UserVM) {
+    if (data) {
+      for (var property in data) {
+        if (data.hasOwnProperty(property))
+          (<any>this)[property] = (<any>data)[property];
+      }
+    }
+  }
+
+  init(_data?: any) {
+    if (_data) {
+      this.userId = _data['userId'];
+      this.userName = _data['userName'];
+      this.password = _data['password'];
+      this.image = _data['image'];
+    }
+  }
+
+  static fromJS(data: any): UserVM {
+    data = typeof data === 'object' ? data : {};
+    let result = new UserVM();
+    result.init(data);
+    return result;
+  }
+  toJSON(data?: any) {
+    data = typeof data === 'object' ? data : {};
+    data['password'] = this.password;
+    data['image'] = this.image;
+    data['userId'] = this.userId;
+    data['userName'] = this.userName;
+    return data;
+  }
 }
 
+export interface IUserVM {
+  userId?: number;
+  userName?: string;
+  password?: string;
+  image?: string;
+}
--- src/app/my-messenger/my-messenger.component.ts
+++ src/app/my-messenger/my-messenger.component.ts
@@ -9,178 +9,185 @@ import { ChannelMessageVM } from '../models/channelMessageVM';
 @Component({
   selector: 'app-my-messenger',
   templateUrl: './my-messenger.component.html',
-  styleUrls: ['./my-messenger.component.css']
+  styleUrls: ['./my-messenger.component.css'],
 })
-export class MyMessengerComponent implements OnChanges, OnInit  {
-user: UserVM =new UserVM();
-@Input() channel: any | undefined;
-channelMessages:ChannelMessageVM[]=[];
-displayMessagesList:any[]=[];
-participantList:any[]=[];
-currentPage:number=1;
-pageSize:number=10;
-isChannelParticipants:boolean=false;
-textareaValue: string = '';
- constructor(private messengerService: MessengerService,
-            private accountService: UserService,
-            private alertService: AlertService            ) {
-              this.accountService.user.subscribe(x => this.user = x);
+export class MyMessengerComponent implements OnChanges, OnInit {
+  user: UserVM = new UserVM();
+  @Input() channel: any | undefined;
+  channelMessages: ChannelMessageVM[] = [];
+  displayMessagesList: any[] = [];
+  participantList: any[] = [];
+  currentPage: number = 1;
+  pageSize: number = 10;
+  isChannelParticipants: boolean = false;
+  textareaValue: string = '';
+  constructor(
+    private messengerService: MessengerService,
+    private accountService: UserService,
+    private alertService: AlertService
+  ) {
+    this.accountService.user.subscribe((x) => (this.user = x));
   }
-  ngOnInit() {
-    }
- 
+  ngOnInit() {}
+
   async ngOnChanges(changes: SimpleChanges) {
-    if(this.channel?.channelId!>0)
-    {
+    if (this.channel?.channelId! > 0) {
       await this.loadChannelparticipant();
       this.getAllChannelMessages(false);
-          this.currentPage=1
-
+      this.currentPage = 1;
     }
   }
-  
-   async loadChannelparticipant() {
-     this.alertService.clear();
-     await this.messengerService.getChannelParticipants(this.channel?.channelId!).toPromise().then(data => {
-       this.participantList=data
-       this.isChannelParticipants=true;
-     },
-       error => {
-         this.alertService.error(error);
-      });          
- }
 
-
-addMessage()
-{
-    var msg: ChannelMessageVM=new ChannelMessageVM();
-    msg.messageContent=this.textareaValue;
-    msg.channelId=this.channel?.channelId;
-    msg.isSeen=false;
-    msg.messageDate=new Date();
-    msg.senderID=this.user.userId;
-   this.messengerService.addCahnelMessage(msg).subscribe(data => {
-     if(data.res)
-     {
-       this.getNewChannelMessages();
-     }
-     console.log(data);  
-  },
-   error => {
-     this.alertService.error(error);
-   });
-   this.textareaValue="";
- }
-
-
-@HostListener('scroll', ['$event'])
-onScroll(event: any) {
-    if (event.target.offsetHeight + event.target.scrollTop >= event.target.scrollHeight - 1) {
-}
-else if(event.target.scrollTop===0 && this.currentPage>=1){
-  if(this.isChannelParticipants)
-  {
-    this.currentPage++;
-    this.getAllChannelMessages(true);
+  async loadChannelparticipant() {
+    this.alertService.clear();
+    await this.messengerService
+      .getChannelParticipants(this.channel?.channelId!)
+      .toPromise()
+      .then(
+        (data) => {
+          this.participantList = data;
+          this.isChannelParticipants = true;
+        },
+        (error) => {
+          this.alertService.error(error);
+        }
+      );
   }
-}
-}
-moveScrollToBottom() {
-  setTimeout(function () {
-    let element = document.getElementById("messages-flow") as HTMLDivElement;
-    if(element!=undefined && element!=null)
-    element.scrollTop = element.scrollHeight;
-  }, 2);
-}
-moveScrollToElement(elementId:any) {
-  setTimeout(function () {
-    let element = document.getElementById(elementId) as HTMLDivElement;
-    if(element!=undefined && element!=null)
-    element.scrollIntoView();
-  }, 2);
-}
-    
-    async getAllChannelMessages(olderMessages:boolean=false)  {
-    let lastChannelMessage : number=0;
-    if(this.currentPage === 1)
-    {
-      this.displayMessagesList=[] as ChannelMessageVM[];
+
+  addMessage() {
+    var msg: ChannelMessageVM = new ChannelMessageVM();
+    msg.messageContent = this.textareaValue;
+    msg.channelId = this.channel?.channelId;
+    msg.isSeen = false;
+    msg.messageDate = new Date();
+    msg.senderID = this.user.userId;
+    this.messengerService.addCahnelMessage(msg).subscribe(
+      (data) => {
+        if (data.res) {
+          this.getNewChannelMessages();
+        }
+        console.log(data);
+      },
+      (error) => {
+        this.alertService.error(error);
+      }
+    );
+    this.textareaValue = '';
+  }
+
+  @HostListener('scroll', ['$event'])
+  onScroll(event: any) {
+    if (
+      event.target.offsetHeight + event.target.scrollTop >=
+      event.target.scrollHeight - 1
+    ) {
+    } else if (event.target.scrollTop === 0 && this.currentPage >= 1) {
+      if (this.isChannelParticipants) {
+        this.currentPage++;
+        this.getAllChannelMessages(true);
+      }
     }
-    else{
-      if(this.displayMessagesList.length>0)
-        lastChannelMessage= this.displayMessagesList[0].channelMessageId;
+  }
+  moveScrollToBottom() {
+    setTimeout(function () {
+      let element = document.getElementById('messages-flow') as HTMLDivElement;
+      if (element != undefined && element != null)
+        element.scrollTop = element.scrollHeight;
+    }, 2);
+  }
+  moveScrollToElement(elementId: any) {
+    setTimeout(function () {
+      let element = document.getElementById(elementId) as HTMLDivElement;
+      if (element != undefined && element != null) element.scrollIntoView();
+    }, 2);
+  }
+
+  async getAllChannelMessages(olderMessages: boolean = false) {
+    let lastChannelMessage: number = 0;
+    if (this.currentPage === 1) {
+      this.displayMessagesList = [] as ChannelMessageVM[];
+    } else {
+      if (this.displayMessagesList.length > 0)
+        lastChannelMessage = this.displayMessagesList[0].channelMessageId;
     }
-    await this.messengerService.getChannelMessages(this.channel?.channelId!, this.currentPage, this.pageSize)
-    .toPromise()
-     .then(response => {
-     if (response.length == 0) {
-              return;
-            }
-            if(olderMessages)
-            {
-              response.forEach((element:any)=>{
-                    const user=this.participantList.find( (cp:any)=> cp.userId==element.senderID); 
-                   if(element.senderId==this.user.userId)
-                    element.isSelf=true;
-                this.displayMessagesList.unshift({
-                 ...element,
-                 ...user
-                });
-               });
-               this.moveScrollToElement("dvmessage_"+lastChannelMessage);
-            }
-            else
-            {
-              response.reverse().forEach((element:any)=>{
-              const user=this.participantList.find( (cp:any)=> cp.userId==element.senderID); 
-                if(element.senderId==this.user.userId)
-                    element.isSelf=true;
-                this.displayMessagesList.push({
-                  ...element,
-                  ...user
-                 });
-               });
-               this.moveScrollToBottom();
-            }
+    await this.messengerService
+      .getChannelMessages(
+        this.channel?.channelId!,
+        this.currentPage,
+        this.pageSize
+      )
+      .toPromise()
+      .then(
+        (response) => {
+          if (response.length == 0) {
+            return;
           }
-            ,
-            (error: any) => {
-              this.alertService.error(error);
+          if (olderMessages) {
+            response.forEach((element: any) => {
+              const user = this.participantList.find(
+                (cp: any) => cp.userId == element.senderID
+              );
+              if (element.senderId == this.user.userId) element.isSelf = true;
+              this.displayMessagesList.unshift({
+                ...element,
+                ...user,
+              });
             });
+            this.moveScrollToElement('dvmessage_' + lastChannelMessage);
+          } else {
+            response.reverse().forEach((element: any) => {
+              const user = this.participantList.find(
+                (cp: any) => cp.userId == element.senderID
+              );
+              if (element.senderId == this.user.userId) element.isSelf = true;
+              this.displayMessagesList.push({
+                ...element,
+                ...user,
+              });
+            });
+            this.moveScrollToBottom();
+          }
+        },
+        (error: any) => {
+          this.alertService.error(error);
+        }
+      );
   }
-   insertNewLine(event: KeyboardEvent): void {
-    if (!event.shiftKey) {
-      // Prevent the default behavior of Enter key
+  getNewChannelMessages() {
+    let lastChannelMessage: number = 0;
+    if (this.displayMessagesList.length > 0)
+      lastChannelMessage =
+        this.displayMessagesList[this.displayMessagesList.length - 1]
+          .channelMessageId;
+    this.messengerService
+      .getNewChannelMessages(this.channel?.channelId!, lastChannelMessage)
+      .toPromise()
+      .then(
+        (response: any) => {
+          if (response!.length == 0) {
+            return;
+          }
+          response.forEach((element: any) => {
+            const user = this.participantList.find(
+              (cp: any) => cp.userId == element.senderID
+            );
+            if (element.senderId == this.user.userId) element.isSelf = true;
+            this.displayMessagesList.push({
+              ...element,
+              ...user,
+            });
+          });
+          this.moveScrollToElement('dvmessage_' + lastChannelMessage);
+        },
+        (error: any) => {
+          this.alertService.error(error);
+        }
+      );
+  }
+  handleKeyPress(event: KeyboardEvent) {
+    if (event.key === 'Enter' && !event.shiftKey) {
       event.preventDefault();
+      this.addMessage();
     }
   }
-  getNewChannelMessages()  {
-    let lastChannelMessage : number=0;
-    if(this.displayMessagesList.length>0)
-      lastChannelMessage= this.displayMessagesList[this.displayMessagesList.length-1].channelMessageId;
-    this.messengerService.getNewChannelMessages(this.channel?.channelId!, lastChannelMessage)
-    .toPromise()
-     .then((response:any) => {
-     if (response!.length == 0) {
-              return;
-            }
-              response.forEach((element:any)=>{
-                    const user=this.participantList.find( (cp:any)=> cp.userId==element.senderID); 
-                if(element.senderId==this.user.userId)
-                    element.isSelf=true;
-                  this.displayMessagesList.push({
-                   ...element,
-                   ...user
-                  });
-               });
-               this.moveScrollToElement("dvmessage_"+lastChannelMessage);
-          }
-            ,
-            (error: any) => {
-                 this.alertService.error(error);
-
-            });
-  }
-
- 
 }
